e14b8f1c74c2550aaa305ecfc7a5111a4a3eb5cb
Zou Yulin <zouy0013@e.ntu.edu.sg>
2020-03-03 16:41:03 +0800

    Initial Commit

---
+++ Lenet-5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lenet-5
+//
+//  Created by 邹钰林 on 3/3/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
